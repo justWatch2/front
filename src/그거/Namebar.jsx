14c9f846225455029082b98cfbdebed9c16de209
jin242 <78089020+jin242@users.noreply.github.com>
2025-05-30 11:48:29 +0900

    Add files via upload

---
+++ src/그거/Namebar.jsx
@@ -0,0 +1,15 @@
+import './menu.css';
+
+function namebar({name}) {
+
+    return (
+        <div>
+            <nav>
+                <h1>{name}</h1>
+            </nav>
+        </div>
+
+    );
+}
+
+export default namebar;
